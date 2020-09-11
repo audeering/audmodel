0c5a81712cab05ec929fe00185be63b94cd96bca
Hagen Wierstorf <hwierstorf@audeering.com>
2020-09-11 13:36:09 +0200

    Fix subgroup extraction from URL

--- audmodel/core/url.py
+++ audmodel/core/url.py
@@ -1,3 +1,10 @@
+import typing
+
+import audfactory
+
+from audmodel.core.define import defaults
+
+
 def name_from_url(url: str) -> str:
     return url.split('/')[-4]
 
@@ -6,8 +13,19 @@ def repository_from_url(url: str) -> str:
     return url.split('/')[4]
 
 
-def subgroup_from_url(url: str) -> str:
-    return '.'.join(url.split('/')[8:-4])
+def subgroup_from_url(url: str) -> typing.Union[None, str]:
+    # Consider length of group ID
+    url_start = (
+        f'{audfactory.config.ARTIFACTORY_ROOT}/'
+        f'repo/'
+        f'{audfactory.group_id_to_path(defaults.GROUP_ID)}'
+    )
+    start_length = len(url_start.split('/'))
+    subgroup = '.'.join(url.split('/')[start_length:-4])
+    if len(subgroup) == 0:
+        return None
+    else:
+        return subgroup
 
 
 def version_from_url(url: str) -> str:
--- tests/test_url.py
+++ tests/test_url.py
@@ -6,6 +6,52 @@ import audfactory
 import audmodel
 
 
+URL = (
+    f'{audfactory.config.ARTIFACTORY_ROOT}/'
+    f'{audmodel.core.define.defaults.REPOSITORY_PUBLIC}'
+)
+
+
+@pytest.mark.parametrize(
+    'group_id,subgroup,name',
+    [
+        (
+            'com.audeering.models',
+            None,
+            'mymodel',
+        ),
+        (
+            'com.audeering.models',
+            'audmodel',
+            'mymodel',
+        ),
+        (
+            'com.audeering.models',
+            'audmodel1.audmodel2',
+            'mymodel',
+        ),
+    ],
+)
+def test_from_url(group_id, subgroup, name):
+    version = '1.0.0'
+    repository = audmodel.core.define.defaults.REPOSITORY_PUBLIC
+    audmodel.core.define.defaults.GROUP_ID = group_id
+    if subgroup is None:
+        group_id = group_id
+    else:
+        group_id = f'{group_id}.{subgroup}'
+    url = (
+        f'{URL}/'
+        f'{audfactory.group_id_to_path(group_id)}/'
+        f'{name}/'
+        f'uid/{version}/uid-{version}.zip'
+    )
+    assert audmodel.core.url.subgroup_from_url(url) == subgroup
+    assert audmodel.core.url.name_from_url(url) == name
+    assert audmodel.core.url.version_from_url(url) == version
+    assert audmodel.core.url.repository_from_url(url) == repository
+
+
 @pytest.mark.usefixtures('create')
 def test_url():
     uid = pytest.UIDS[0]
