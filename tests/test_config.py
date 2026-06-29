67a4c159f4ee04176a6e78f2a309c4d37a76e723
Hagen Wierstorf <hwierstorf@audeering.com>
2026-06-29 16:51:55 +0200

    Fix test under Windows

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 import yaml
 
@@ -61,7 +63,7 @@ def test_config_file(tmpdir):
         cf.write("repositories:\n")
     error_msg = (
         "You cannot specify an empty 'repositories:' section "
-        f"in the configuration file '{config_file}'."
+        f"in the configuration file '{re.escape(config_file)}'."
     )
     with pytest.raises(ValueError, match=error_msg):
         audmodel.core.config.load_configuration_file(config_file)
